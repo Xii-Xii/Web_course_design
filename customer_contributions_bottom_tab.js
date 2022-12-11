337fbb426d42f3e58c3af4cae8e5fdbe560b3ed3
Xii-Xii <99240815+Xii-Xii@users.noreply.github.com>
2022-12-11 16:49:12 +0800

    Delete customer_contributions_bottom_tab.js

--- customer_contributions_bottom_tab.js
+++
@@ -1,9 +0,0 @@
-function $(id){
-    return document.getElementById(id);
-}
-function ccb_tab(id){
-    for(var i of($('customer_contributions_bottom_content').children[0].children)){
-        i.style.display="none";
-    }
-    $(id+"_content").style.display="block";
-}
